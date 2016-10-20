732498729a111a71bdef0f9a140315a25f51372f
Nathan Mc Grath <mcgrath.nathan@yahoo.ie>
2016-10-20 10:35:28 +0100

    removed core-js types

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 // polyfill
-require('core-js');
-require('reflect-metadata');
-require('zone.js');
-require('web-animations-js');
+import 'core-js';
+import 'reflect-metadata';
+import 'web-animations-js';
+
+// dependencies
+import 'zone.js';
 
 // app
-require('./main.scss');
+import './main.scss';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
