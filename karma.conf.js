b0a084b2747fb86cec79de5d6e2a43e26b524bbe
Nathan Mc Grath <contact@nathan-mcgrath.com>
2017-06-05 14:20:48 +0100

    added basic karma configuration

---
+++ karma.conf.js
@@ -0,0 +1,37 @@
+
+module.exports = function(config) {
+  config.set({
+    basePath: 'packages/ui-components',
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      {
+        pattern: 'components/**/*.+(ts|html)'
+      }
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      '**/*.ts': ['karma-typescript']
+    },
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        transforms: [
+          require('karma-typescript-angular2-transform')
+        ]
+      },
+      compilerOptions: {
+        lib: ['ES2015', 'DOM']
+      },
+      tsconfig: 'tsconfig.test.json'
+    },
+    reporters: ['progress', 'karma-typescript'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    concurrency: Infinity
+  })
+}
